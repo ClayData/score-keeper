ff65dce4275205586332e3872b1567dab81fe732
ClayData <claytonortiz96@gmail.com>
2020-02-12 13:09:38 -0600

    added game stats ajax call

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $("#submit").on("click", function (){
 	var yearInput = $("#exampleFormControlSelect1").val();
 	var monthInput = $("#month-input").val();
 	var dayInput = $("#day-input").val(); 
-	console.log(yearInput)
+	
 	displayNbaScores(yearInput, monthInput, dayInput);
 })
 
@@ -36,13 +36,15 @@ function displayNbaScores(y, m, d){
 	
 	for(var i = 0; i < results.length; i++){
 		//create gameid array
-		gameIdArr.push(results[i].gameId);
+		
 		//Home scores and teams
-		var homeBox = $("<div>").attr("class", "list-group box-score");
+		var homeBox = $("<div>").attr({"class": "list-group box-score"});
 		var homeTeam = $("<button>").attr({"class": "list-group-item list-group-item-action",
 										   "type": "button",
 										   "data-toggle": "modal",
-										   "data-target": "#myModal"});
+										   "data-target": "#myModal",
+										   "data-gameId": results[i].gameId,
+										   "data-aVh": 1});
 		
 		//get home team name and remove quotes from string
 		var homeText = JSON.stringify(results[i].hTeam.fullName);
@@ -65,7 +67,9 @@ function displayNbaScores(y, m, d){
 		var awayTeam = $("<button>").attr({"class": "list-group-item list-group-item-action",
 		"type": "button",
 		"data-toggle": "modal",
-		"data-target": "#myModal"});
+		"data-target": "#myModal",
+		"data-gameId": results[i].gameId,
+		"data-aVh": 0});
 		
 		var awayText = JSON.stringify(results[i].vTeam.fullName);
 		var cleanAwayText = awayText.replace(/['"]/g, '')
@@ -101,9 +105,59 @@ function displayNbaScores(y, m, d){
 	// scoreUpdater(y, m, d);
 }
 
-function teamStatsCall(){
+var teamIds = {"Atlanta Hawks": "1",
+			   "Boston Celtics": "2",
+			   "Brooklyn Nets": "4",
+			   "Charlotte Hornets": "5",
+			   "Chicago Bulls": "6",
+			   "Cleveland Cavaliers": "7",
+			   "Dallas Mavericks": "8",
+			   "Denver Nuggets": "9",
+			   "Detroit Pistons": "10",
+			   "Golden State Warriors": "11",
+			   "Houston Rockets": "14",
+			   "Indiana Pacers": "15",
+			   "Los Angeles Clippers": "16",
+			   "Los Angeles Lakers": "17",
+			   "Memphis Grizzles": "19",
+			   "Miami Heat": "20",
+			   "Milwaukee Bucks": "21",
+			   "Minnesota Timberwolves": "22",
+			   "New Orleans Pelicans": "23",
+			   "New York Knicks": "24",
+			   "Oklahoma City Thunder": "25",
+			   "Orlando Magic": "26",
+			   "Philadelphia 76ers": "27",
+			   "Phoenix Suns": "28",
+			   "Portland Trailblazers": "29",
+			   "Sacramento Kings": "30",
+			   "San Antonio Spurs": "31",
+			   "Toranto Raptors": "38",
+			   "Utah Jazz": "40",
+			   "Washington Wizards": "41"}
 
-}
+$(document).on("click", ".list-group-item-action", function (){
+	
+	var id = $(this).attr("data-gameId");
+	var aVh = $(this).attr("data-aVh");
+	console.log(aVh);
+
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://api-nba-v1.p.rapidapi.com/statistics/games/gameId/" + id,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+			"x-rapidapi-key": "caaa1aa51cmsh34aeffc6045e01dp1cb5e9jsnff5f9d0222cc"
+		}
+	}
+	
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+
+	});
+})
 
 
 
